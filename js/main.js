2346c51bdc611dd3e68f523fe104d90581673c59
inverse231 <3xinverse@gmail.com>
2020-03-11 22:43:17 +0300

    second hw

--- js/main.js
+++ js/main.js
@@ -1,22 +1,73 @@
-const products = [
-    {id: 1, title: 'Notebook', price: 2000},
-    {id: 2, title: 'Mouse', price: 20},
-    {id: 3, title: 'Keyboard', price: 200},
-    {id: 4, title: 'Gamepad', price: 50},
-];
+class ProductsList{
+    constructor(container = '.products'){
+        this.container = container;
+        this.goods = [];
+        this.allProducts = [];
+        this._fetchProducts();
+    } 
+    
+    _fetchProducts(){
+        this.goods = [
+            {id: 1, title: 'Notebook', price: 2000},
+            {id: 2, title: 'Mouse', price: 20},
+            {id: 3, title: 'Keyboard', price: 200},
+            {id: 4, title: 'Gamepad', price: 50},
+        ];
+    }
+    countSum(){
+        let sum = 0;
+        this.goods.forEach(element => {
+            sum += element.price;
+        });
+        return sum;
+    }
+    render() {
+        const block = document.querySelector(this.container);
+        for(let product of this.goods){
+            const productObj = new ProductItem(product);
+            this.allProducts.push(productObj);
+            block.insertAdjacentHTML('beforeend',productObj.render())
+        }
+    }
+    
+}
 
-const renderProduct = (title="Item", price="150") => {
-    return `<div class="product-item">
-                <h3>${title}</h3>
-                <p>${price}</p>
-                <button class="buy-btn">Buy</button>
+class ProductItem{
+	constructor(product, img = 'https://placehold.it/200x150'){
+		this.title = product.title;
+		this.price = product.price;
+		this.id = product.id;
+		this.img = img;
+		
+	}
+	
+	render(){
+		 return `<div class="product-item" data-id="${this.id}">
+                <img src="${this.img}" alt="Some img">
+                <h3>${this.title}</h3>
+                <p>${this.price}</p>
+                <button class="buy-btn">Купить</button>
             </div>`
-};
-const renderPage = (list=[]) => {
-    const productsList = list.map(({title, price}) => renderProduct(title, price));
-    productsList.forEach(element => {
-        document.querySelector('.products').innerHTML += element;
-    });
-};
+	}
+}
+class CartItem {
+    constructor(title, price) {
 
-renderPage(products);
+    }
+    render() {
+        return
+    }
+}
+
+class Cart {
+    constructor() {
+        this.items = [];
+    }
+    addCartItem(item) { }
+    deleteCartItem(item) { }
+    changeItemQuantity(item) { }
+}
+
+let list = new ProductsList();
+console.log(list.countSum())
+list.render();
